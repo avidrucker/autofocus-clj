efb0e6a5d721ed680063a022962b402766e83ca7
avidrucker <avidrucker@gmail.com>
2023-02-13 17:56:48 -0500

    docs clarify comments, docstrings, to-do's

--- src/af/af.clj
+++ src/af/af.clj
@@ -6,25 +6,26 @@
    ;; [clojure.tools.cli :as cli]
    ))
 
-;; TODO: create af.cli namespace for cli specific functionality
-;; TODO: have the user enter any key when in a non-interactive page to return back to the main menu, rather than simply 'returning' immediately, so as to give the user time to read said page's contents first w/o the visual noise of also seeing the list and menu again
+;; DONE: create af.cli namespace for cli specific functionality
+;; TODO: enable the user to press *any* key when in a non-interactive page to return back to the main menu, rather than simply 'returning' immediately, so as to give the user time to read said page's contents first w/o the visual noise of also seeing the list and menu again
+;; DONE: have the user press the ENTER key for "non-interactive" pages to return back to the main menu
 
-
-;;;; TODO: implement console clearing
+;; DONE: implement console clearing in the terminal
+;; TODO: implement console clearing in the REPL
 
 
   ;; Q: Is this file this project's "entry point"?
   ;; Q: What is the difference in purpose/intent between af.af and af.core, as intended by the authors of tools.cli?
-;; Q: What exactly is the point of the exec function?
+;; Q: What exactly is the point of the exec function in contrast with -main?
 (defn exec
-  "Invoke me with clojure -X af.entry-point/exec"
+  "Invoke me with `clojure -X af.entry-point/exec`"
   [opts]
   (println "exec with" opts))
 
 
 ;; Q: What are the meaningful differences and similarities in purpose/role/functionality between the exec and main functions?
 (defn -main
-  "Invoke me with clojure -M -m af.entry-point"
+  "Invoke me with `clojure -M -m af.entry-point` or `clj -M -m af.af`"
   [& args]
   ;; (println "-main with" args)
   ;; (let [input (read-line)]
