a01108b987aa887b8c944a991eba777baa3e366a
avidrucker <avidrucker@gmail.com>
2023-02-26 15:42:46 -0500

    refactor tidy ns

--- src/af/af.clj
+++ src/af/af.clj
@@ -7,17 +7,15 @@
    ))
 
 ;; DONE: create af.cli namespace for cli specific functionality
-;; TODO: enable the user to press *any* key when in a non-interactive page to return back to the main menu, rather than simply 'returning' immediately, so as to give the user time to read said page's contents first w/o the visual noise of also seeing the list and menu again
 ;; DONE: have the user press the ENTER key for "non-interactive" pages to return back to the main menu
-
 ;; DONE: implement console clearing in the terminal
 ;; TODO: implement console clearing in the REPL
 
 
   ;; Q: Is this file this project's "entry point"?
-  ;; Q: What is the difference in purpose/intent between af.af and af.core, as intended by the authors of tools.cli?
+  ;; Q: What is the difference in purpose/intent between af.af and af.core, as intended by the authors of tools.cli, or the authors of neil?
 ;; Q: What exactly is the point of the exec function in contrast with -main?
-(defn exec
+#_(defn exec
   "Invoke me with `clojure -X af.entry-point/exec`"
   [opts]
   (println "exec with" opts))
@@ -27,10 +25,5 @@
 (defn -main
   "Invoke me with `clojure -M -m af.entry-point` or `clj -M -m af.af`"
   [& args]
-  ;; (println "-main with" args)
-  ;; (let [input (read-line)]
-  ;;   (if (or (= input "yes") (= input "no"))
-  ;;     (println "Thanks for your input")
-  ;;     (println "Invalid input. Please enter either 'yes' or 'no'")))
   (cli/cli-do-app-cycles))
 
